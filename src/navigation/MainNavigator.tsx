25755c6a74e6dd69da624308be9dd211afcbfb58
almonsour13 <almonsoursalida@gmail.com>
2026-05-07 15:02:57 +0800

    feat:added custom main tab bar and activity button

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,20 +1,27 @@
 import HomeScreen from "@/features/home/HomeScreen";
+import MainTabBar from "@/shared/components/layout/MainTabBar";
+import { useActivity } from "@/shared/hooks/use-activity";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Tab = createBottomTabNavigator();
 
 export default function MainNavigator() {
+    useActivity();
     return (
-        <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-                sceneStyle: {
-                    backgroundColor: "transparent",
-                },
-            }}
-        >
-            <Tab.Screen name="Home" component={HomeScreen} />
-        </Tab.Navigator>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Tab.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    sceneStyle: {
+                        backgroundColor: "transparent",
+                    },
+                }}
+                tabBar={(props) => <MainTabBar {...props} />}
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+            </Tab.Navigator>
+        </SafeAreaView>
     );
 }
---
+++ src/shared/components/ActivityButton.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/shared/utils/cn";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity, View } from "react-native";
+import Card from "./ui/Card";
+
+export default function ActivityButton() {
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity activeOpacity={0.9}>
+            <Card
+                className={cn(
+                    "relative bg-primary h-20 aspect-square justify-center items-center ",
+                )}
+            >
+                <View className="absolute justify-between items-center">
+                    <Ionicons
+                        name="footsteps"
+                        size={28}
+                        className="text-white"
+                    />
+                </View>
+            </Card>
+        </TouchableOpacity>
+    );
+}
