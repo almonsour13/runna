5ede5ae16793840a507f5da2a3999ab1129f7729
almonsour13 <almonsoursalida@gmail.com>
2026-05-09 01:38:03 +0800

    added storage keys

---
+++ src/shared/constant/constant.ts
@@ -0,0 +1,19 @@
+import { LocationAccuracy } from "expo-location";
+
+export const LOCATION_TIME_INTERVAL_MS = 3000;
+export const LOCATION_ACCURACY = LocationAccuracy.BestForNavigation;
+export const DISTANCE_INTERVAL_METERS = 5;
+
+export const MIN_ACCURACY_METERS = 15;
+export const MIN_DISTANCE_METERS = 5;
+export const MAX_SPEED_MPS = 10;
+export const WARMUP_READINGS = 5;
+
+const PREFIX = "@runna/test";
+
+export const STORAGE_KEYS = {
+    activity: `${PREFIX}:activity`,
+    activityTracking: `${PREFIX}:activityTracking`,
+    profile: `${PREFIX}:profile`,
+    settings: `${PREFIX}:settings`,
+};
