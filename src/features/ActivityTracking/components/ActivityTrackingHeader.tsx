2c59d93d10e4f441b637161b2564f8f1160f9de0
almonsour13 <almonsoursalida@gmail.com>
2026-05-08 16:00:06 +0800

    feat: created activity tracking header

---
+++ src/features/ActivityTracking/components/ActivityTrackingHeader.tsx
@@ -0,0 +1,24 @@
+import { RowView } from "@/shared/components/CustomView";
+import Text from "@/shared/components/ui/Text";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
+
+export default function ActivityTrackingHeader() {
+    const navigation = useNavigation();
+    return (
+        <RowView className="px-4 pt-4 gap-2 items-center">
+            <RowView className="gap-4 items-center">
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Ionicons name="arrow-back" size={24} />
+                </TouchableOpacity>
+            </RowView>
+            <RowView className="flex-1 justify-center items-center">
+                <Text>Morning Walk</Text>
+            </RowView>
+            <RowView>
+                <TouchableOpacity></TouchableOpacity>
+            </RowView>
+        </RowView>
+    );
+}
