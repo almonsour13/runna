cb6efbaaa57a13ffc1bea0d1d98a2f3696776ddd
almonsour13 <almonsoursalida@gmail.com>
2026-05-21 01:04:26 +0800

    feat: implement ScheduleService for managing schedule data with CRUD operations

---
+++ src/shared/services/storage/schedule.service.ts
@@ -0,0 +1,73 @@
+import { db } from "@/shared/db";
+import { schedule } from "@/shared/db/schema";
+import { Schedule } from "@/shared/types/type";
+import { logger } from "@/shared/utils/logger";
+import { eq } from "drizzle-orm";
+
+class ScheduleService {
+    async get(): Promise<Schedule[]> {
+        try {
+            const data = await db.select().from(schedule);
+            logger.log("[ScheduleStorage] get → success");
+            return data;
+        } catch (error) {
+            logger.error("[ScheduleStorage] get → error:", error);
+            throw error;
+        }
+    }
+    async getById(id: string): Promise<Schedule | null> {
+        try {
+            const data = await db
+                .select()
+                .from(schedule)
+                .where(eq(schedule.id, id));
+            logger.log("[ScheduleStorage] getById → success");
+            return data[0] || null;
+        } catch (error) {
+            logger.error("[ScheduleStorage] getById → error:", error);
+            throw error;
+        }
+    }
+    async create(scheduleInput: Omit<Schedule, "status">) {
+        try {
+            const data = await db
+                .insert(schedule)
+                .values(scheduleInput)
+                .returning();
+            logger.log("[ScheduleStorage] create → success");
+            return data[0];
+        } catch (error) {
+            logger.error("[ScheduleStorage] create → error:", error);
+            throw error;
+        }
+    }
+    async update(id: string, partial: Partial<Schedule>) {
+        try {
+            await db.update(schedule).set(partial).where(eq(schedule.id, id));
+            logger.log("[ScheduleStorage] update → success");
+        } catch (error) {
+            logger.error("[ScheduleStorage] update → error:", error);
+            throw error;
+        }
+    }
+    async delete(id: string) {
+        try {
+            await db.delete(schedule).where(eq(schedule.id, id));
+            logger.log("[ScheduleStorage] delete → success");
+        } catch (error) {
+            logger.error("[ScheduleStorage] delete → error:", error);
+            throw error;
+        }
+    }
+    async clear() {
+        try {
+            await db.delete(schedule);
+            logger.log("[ScheduleStorage] clear → success");
+        } catch (error) {
+            logger.error("[ScheduleStorage] clear → error:", error);
+            throw error;
+        }
+    }
+}
+
+export const scheduleService = new ScheduleService();
